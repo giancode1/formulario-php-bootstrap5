344eb2b76e389265ed3efcbb7fa85da9a1231aba
gianccool <65518977+giancode1@users.noreply.github.com>
2021-07-11 20:03:13 -0500

    Update php-mysql-connect.php

--- php-mysql-connect.php
+++ php-mysql-connect.php
@@ -1,6 +1,6 @@
 <?php
 //php para probar si se conecta a una base de datos.
-  $conn = new mysqli("localhost", "giandatos", "datos", "base0");
+  $conn = new mysqli("localhost", "usuario", "pass", "db_name");
   
   if ($conn->connect_error) {
     die("ERROR: Unable to connect: " . $conn->connect_error);
